8fe1a659b95ae02f74866ebbbc8c299b7ca41fda
benfector <benfectordd@proton.me>
2025-11-11 00:16:31 -0300

    Update rsa.cpp

--- src/rsa.cpp
+++ src/rsa.cpp
@@ -1,4 +1,4 @@
-/*g++ -std=c++17 -Iinclude src/main.cpp src/rsa.cpp -o rsa_program -lgmp -lgmpxx -lstdc++fs*/
+/*g++ -std=c++17 -Iinclude src/main.cpp src/rsa.cpp -o rsa_program -lgmp -lgmpxx*/
 
 /*rsa.cpp*/
 
